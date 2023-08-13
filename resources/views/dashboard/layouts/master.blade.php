b5541706478e26d87019cac996099cef23b2d508
omar elruby <elrubyomar@gmail.com>
2023-08-13 13:41:43 +0300

    remove copyright

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -268,9 +268,9 @@
 @yield('content')<!-- ////////////////////////////////////////////////////////////////////////////-->
 
 
-<footer class="footer footer-static footer-light navbar-border">
-    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright  &copy; 2023 <a class="text-bold-800 grey darken-2" href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT </a>, All rights reserved. </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span></p>
-</footer>
+{{--<footer class="footer footer-static footer-light navbar-border">--}}
+{{--    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">Copyright  &copy; 2023 <a class="text-bold-800 grey darken-2" href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT </a>, All rights reserved. </span><span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span></p>--}}
+{{--</footer>--}}
 
 <!-- BEGIN VENDOR JS-->
 <script src="{{asset('app-assets/vendors/js/vendors.min.js')}}"></script>
