2e6f3f58f324da46eecbcec50b09754b1c3b419c
omar elruby <elrubyomar@gmail.com>
2023-09-04 12:03:20 +0300

    edit on customer controller

--- resources/views/mails/forgetPassword.blade.php
+++ resources/views/mails/forgetPassword.blade.php
@@ -1,4 +1,4 @@
- <h2>{{'Welcome Dear'.($data['name'])}}</h2>
+ <h2>{{'Welcome Dear'.' '.($data['name'])}}</h2>
  <p>
-     To Reset Password Go to This Link <a href="https://zahra-academy.netlify.app/forget_password/.{{($data['code'])}}./.{{($data['email'])}}" style="color: blue;">LINK</a>
+     To Reset Password Go to This Link <a href="https://zahra-academy.netlify.app/forget_password/{{($data['code'])}}/{{($data['email'])}}" style="color: blue;">LINK</a>
  </p>
