fc204a56a444195dbb272aef79c35fba24292eda
omar elruby <elrubyomar@gmail.com>
2023-09-10 17:29:45 +0300

    edit appointments

--- app/Http/Resources/SessionAppointmentsResource.php
+++ app/Http/Resources/SessionAppointmentsResource.php
@@ -18,7 +18,7 @@ class SessionAppointmentsResource extends JsonResource
             'id' => $this->id,
             'date' => $this->date,
             'month' => $this->month,
-            'time' => explode(' ',date('h:i:s', strtotime($this->date)))[1],
+            'time' =>  date('h:i:s', strtotime($this->date)) ,
             'available' => $this->booking ? false : (($this->date > date('Y-m-d H:i')) ? true : false),
 
         ];
