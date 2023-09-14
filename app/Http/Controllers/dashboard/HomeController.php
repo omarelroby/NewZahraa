702fc048e9a473685732272f033304efbe1fe8fe
omar elruby <elrubyomar@gmail.com>
2023-09-14 03:53:45 +0300

    show icons in home dashboard

--- app/Http/Controllers/dashboard/HomeController.php
+++ app/Http/Controllers/dashboard/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\dashboard;
 
+use App\Models\Course;
+use App\Models\Ebook;
+use App\Models\FreeVideo;
+use App\Models\OnlineCourse;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,7 +15,11 @@ class HomeController extends BaseController
 
 {
     public function index(){
-        return view('dashboard.home');
+        $ebooks=Ebook::all()->count();
+        $courses=Course::all()->count();
+        $free_videos=FreeVideo::all()->count();
+        $online_course=OnlineCourse::all()->count();
+        return view('dashboard.home',compact('ebooks','courses','free_videos','online_course'));
     }
     public function create(){
         return view('dashboard.create');
--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,5 +1,75 @@
 @extends('dashboard.layouts.master')
 @section('content')
-     welcome
+
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+
+                        <div class="row" style="margin-left: 15%;margin-right: 15%;">
+                            <div class="col-lg-3 col-sm-12 border-right-blue-grey border-right-lighten-5">
+                                <div class="pb-1">
+                                    <div class="clearfix mb-1">
+                                        <i class="icon-book-open font-large-1 blue-grey float-left mt-1"></i>
+                                        <span class="font-large-2 text-bold-300 info float-right">{{$ebooks}}</span>
+                                    </div>
+                                    <div class="clearfix">
+                                        <span class="text-muted">{{__('dashboard.ebooks')}}</span>
+{{--                                        <span class="info float-right"><i class="ft-arrow-up info"></i> 16.89%</span>--}}
+                                    </div>
+                                </div>
+                                <div class="progress mb-0" style="height: 7px;">
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-12 border-right-blue-grey border-right-lighten-5">
+                                <div class="pb-1">
+                                    <div class="clearfix mb-1">
+                                        <i class="icon-folder-alt font-large-1 blue-grey float-left mt-1"></i>
+                                        <span class="font-large-2 text-bold-300 danger float-right">{{$courses}}</span>
+                                    </div>
+                                    <div class="clearfix">
+                                        <span class="text-muted">{{__('dashboard.courses')}}</span>
+{{--                                        <span class="danger float-right"><i class="ft-arrow-up danger"></i> 5.14%</span>--}}
+                                    </div>
+                                </div>
+                                <div class="progress mb-0" style="height: 7px;">
+                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 45%" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-12 border-right-blue-grey border-right-lighten-5">
+                                <div class="pb-1">
+                                    <div class="clearfix mb-1">
+                                        <i class="icon-screen-desktop font-large-1 blue-grey float-left mt-1"></i>
+                                        <span class="font-large-2 text-bold-300 success float-right">{{$online_course}}</span>
+                                    </div>
+                                    <div class="clearfix">
+                                        <span class="text-muted">{{__('dashboard.online-courses')}}</span>
+{{--                                        <span class="success float-right"><i class="ft-arrow-down success"></i> 2.24%</span>--}}
+                                    </div>
+                                </div>
+                                <div class="progress mb-0" style="height: 7px;">
+                                    <div class="progress-bar bg-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-12">
+                                <div class="pb-1">
+                                    <div class="clearfix mb-1">
+                                        <i class="icon-social-youtube font-large-1 blue-grey float-left mt-1"></i>
+                                        <span class="font-large-2 text-bold-300 warning float-right">{{$free_videos}}</span>
+                                    </div>
+                                    <div class="clearfix">
+                                        <span class="text-muted">{{__('dashboard.free-videos')}}</span>
+{{--                                        <span class="warning float-right"><i class="ft-arrow-up warning"></i> 43.84%</span>--}}
+                                    </div>
+                                </div>
+                                <div class="progress mb-0" style="height: 7px;">
+                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
 @endsection
