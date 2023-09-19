536241357025a02df86debaa4ee940cec2ff73d6
omar elruby <elrubyomar@gmail.com>
2023-09-19 14:18:11 +0300

    edit on online course group

--- app/Http/Resources/Customer2Resource.php
+++ app/Http/Resources/Customer2Resource.php
@@ -4,25 +4,22 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class GroupNew2Resource extends JsonResource
+class Customer2Resource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'start_date'=>$this->start_date,
-            'end_date'=>$this->end_date,
-            'quizes_number'=>$this->quiz->count(),
-            'students'=>Customer2Resource::collection($this->students),
+            'name' => $this->name,
+            'email' => $this->email,
 
 
         ];
     }
-
 }
