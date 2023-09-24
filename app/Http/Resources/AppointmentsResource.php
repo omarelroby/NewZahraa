4d0ff52a25b65824d370d101d47e7766e5d52b32
omar elruby <elrubyomar@gmail.com>
2023-09-24 23:43:52 +0300

    edit instrucor requests

--- app/Http/Resources/AppointmentsResource.php
+++ app/Http/Resources/AppointmentsResource.php
@@ -19,6 +19,7 @@ class AppointmentsResource extends JsonResource
             'appointment_date' => $this->appointment_date,
             'start_url' => $this->start_url,
             'time' =>  date('h:i:s a', strtotime($this->appointment_date)) ,
+            'group_name' => $this->groups->name??'',
             'course_name' => $this->groups->online_courses->name??'',
             'number_of_students'=>$this->groups->orders->count()??'',
 
