7ad4029d56b72201f02250aa6bb269b88fb26de9
Muhammad Medhat <muhammadmedhat12@gmail.com>
2023-10-07 23:08:21 +0300

    responsive

--- app/DataTables/EbookDataTable.php
+++ app/DataTables/EbookDataTable.php
@@ -69,6 +69,9 @@ class EbookDataTable extends DataTable
                     ->minifiedAjax()
                      ->orderBy(0)
                     ->selectStyleSingle()
+                    ->parameters([
+                        'responsive' => true,
+                      ])
                     ->buttons([
                         Button::make('excel'),
                         Button::make('csv'),
